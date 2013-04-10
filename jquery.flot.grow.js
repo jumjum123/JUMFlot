28cfdab48e5a6b7114e7005b1283913d96e562aa
Thodoris Greasidis <thgreasi@gmail.com>
2013-04-11 00:09:19 +0300

    Fixed bug with flot.resize & improved heavy plots
    Prettified & JSLinted the code.
    Fixed a bug with 'flot.resize' in which if the resize occurred before the end of 'grow' animation, the animation of the plot would continue far beyond the max height of the canvas.
    
    Shortlog:
    -- Changed setInterval with recursive setTimeout measuring the timediff
       (Helps with plots that each redraw takes more time than the Interval,
        and should also make easier to implement requestAnimationFrame in near future).
    -- Changed function name growing to growingLoop
       (JSLint suggested and also was required for the setTimeout implementation).
    -- If the 'resize' plugin is available and a 'resize' occurs before the end of
       the 'grow' animation, then the animation is halted and the plot is
       immediately redrawn to its final state.
       (added binding and unbinding of 'resize' event in processbindEvents and shutdown).

--- jquery.flot.grow.js
+++ jquery.flot.grow.js
@@ -22,23 +22,23 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-(function ($){
+(function ($) {
     "use strict";
     var pluginName = "grow", pluginVersion = "0.4";
-    var options ={
+    var options = {
         series: {
             grow: {
                 active: false,
                 stepDelay: 20,
-                steps:100,
-                growings:[
+                steps: 100,
+                growings: [
                     {
                         valueIndex: 1,
                         stepMode: "linear",
                         stepDirection: "up"
-                    }                    
+                    }
                 ],
-                debug:{active:false,createDocuTemplate: null}
+                debug: { active: false, createDocuTemplate: null }
             }
         }
     };
@@ -52,119 +52,158 @@ THE SOFTWARE.
         var valueIndex;
         plot.hooks.bindEvents.push(processbindEvents);
         plot.hooks.drawSeries.push(processSeries);
-        function createDocuTemplate(){
-            var z,frm;
+        plot.hooks.shutdown.push(shutdown);
+        function createDocuTemplate() {
+            var z, frm;
             z = $.plot.JUMExample.docuObjectToTemplate(
-                [ {name:"data",tree:serie.data},
-                {name:"options.series.grow",tree:options.series.grow,takeDefault:true},
-                {name:"options.series.grow",tree:opt.series.grow},
-                {name:"options.series.editMode",tree:options.series.editMode,takeDefault:true},
-                {name:"options.series.editMode",tree:opt.series.editMode},
-                {name:"options.series.nearBy",tree:options.series.nearBy,takeDefault:true},
-                {name:"options.series.nearBy",tree:opt.series.nearBy}
-                ],pluginName); 
-            $.plot.JUMExample.extendDocuObject(z,pluginName);
-            frm = $.plot.JUMExample.docuObjectToEdit(z,"");
-            return { data:z, form:frm};
-        }		
-        function processSeries(plot, canvascontext, series){
+                [{ name: "data", tree: serie.data },
+                { name: "options.series.grow", tree: options.series.grow, takeDefault: true },
+                { name: "options.series.grow", tree: opt.series.grow },
+                { name: "options.series.editMode", tree: options.series.editMode, takeDefault: true },
+                { name: "options.series.editMode", tree: opt.series.editMode },
+                { name: "options.series.nearBy", tree: options.series.nearBy, takeDefault: true },
+                { name: "options.series.nearBy", tree: opt.series.nearBy }
+                ], pluginName);
+            $.plot.JUMExample.extendDocuObject(z, pluginName);
+            frm = $.plot.JUMExample.docuObjectToEdit(z, "");
+            return { data: z, form: frm };
+        }
+        function processSeries(plot, canvascontext, series) {
             opt = plot.getOptions();
             valueIndex = opt.series.grow.valueIndex;
-            if(opt.series.grow.active === true){
-                if(opt.series.grow.debug.active === true) { 
+            if (opt.series.grow.active === true) {
+                if (opt.series.grow.debug.active === true) {
                     serie = series;
                     opt.series.grow.debug.createDocuTemplate = createDocuTemplate;
                 }
-                if (done === false){
+                if (done === false) {
                     data = plot.getData();
                     data.actualStep = 0;
                     data.growingIndex = 0;
-                    for (var j = 0; j < data.length; j++){
+                    for (var j = 0; j < data.length; j++) {
                         data[j].dataOrg = clone(data[j].data);
-                        for (var i = 0; i < data[j].data.length; i++){ data[j].data[i][valueIndex] = 0;}
+                        for (var i = 0; i < data[j].data.length; i++) { data[j].data[i][valueIndex] = 0; }
                     }
                     plot.setData(data);
                     done = true;
                 }
             }
         }
-        function processbindEvents(plot,eventHolder){
+        function processbindEvents(plot, eventHolder) {
             opt = plot.getOptions();
-            if (opt.series.grow.active === true){
+            if (opt.series.grow.active === true) {
                 var d = plot.getData();
                 for (var j = 0; j < data.length; j++) {
                     opt.series.grow.steps = Math.max(opt.series.grow.steps, d[j].grow.steps);
                 }
-                if(opt.series.grow.stepDelay === 0){ opt.series.grow.stepDelay++;}
-                growfunc = window.setInterval(growing, opt.series.grow.stepDelay);
+                if (opt.series.grow.stepDelay === 0) { opt.series.grow.stepDelay++; }
+                growfunc = window.setTimeout(growingLoop, opt.series.grow.stepDelay);
+
+                if (isPluginRegistered("resize")) {
+                    plot.getPlaceholder().bind("resize", onResize);
+                }
             }
         }
-        function growing(){
+
+        function growingLoop() {
+            var startTime = new Date();
             var growing;
-            if (data.actualStep < opt.series.grow.steps){
+            if (data.actualStep < opt.series.grow.steps) {
                 data.actualStep++;
-                for(var j = 0; j < data.length; j++){
-                    for(var g = 0; g < data[j].grow.growings.length; g++){
+                for (var j = 0; j < data.length; j++) {
+                    for (var g = 0; g < data[j].grow.growings.length; g++) {
                         growing = data[j].grow.growings[g];
-                        if (typeof growing.stepMode === "function"){
-                            growing.stepMode(data[j],data.actualStep,growing);
+                        if (typeof growing.stepMode === "function") {
+                            growing.stepMode(data[j], data.actualStep, growing);
                         }
-                        else{
-                            if (growing.stepMode === "linear"){ growLinear();}
-                            else if (growing.stepMode === "maximum"){ growMaximum();}
-                            else if (growing.stepMode === "delay"){ growDelay();}
-                            else{growNone();}
+                        else {
+                            if (growing.stepMode === "linear") { growLinear(); }
+                            else if (growing.stepMode === "maximum") { growMaximum(); }
+                            else if (growing.stepMode === "delay") { growDelay(); }
+                            else { growNone(); }
                         }
                     }
                 }
                 plt.setData(data);
                 plt.draw();
+
+                var timeDiff = new Date() - startTime;
+                var nextStepDelay = Math.max(0, opt.series.grow.stepDelay - timeDiff);
+                growfunc = window.setTimeout(growingLoop, nextStepDelay);
+            } else {
+                growfunc = null;
             }
-            else{ window.clearInterval(growfunc); }
-            function growNone(){
-                if (data.actualStep === 1){
-                    for (var i = 0; i < data[j].data.length; i++){
+            function growNone() {
+                if (data.actualStep === 1) {
+                    for (var i = 0; i < data[j].data.length; i++) {
                         data[j].data[i][valueIndex] = data[j].dataOrg[i][growing.valueIndex];
                     }
                 }
             }
-            function growLinear(){
-                if (data.actualStep <= data[j].grow.steps){
-                    for (var i = 0; i < data[j].data.length; i++){	
-                        if (growing.stepDirection === "up"){	
+            function growLinear() {
+                if (data.actualStep <= data[j].grow.steps) {
+                    for (var i = 0; i < data[j].data.length; i++) {
+                        if (growing.stepDirection === "up") {
                             data[j].data[i][growing.valueIndex] = data[j].dataOrg[i][growing.valueIndex] / data[j].grow.steps * data.actualStep;
                         }
-                        else if(growing.stepDirection === "down"){
-                            data[j].data[i][growing.valueIndex] = data[j].dataOrg[i][growing.valueIndex] + (data[j].yaxis.max - data[j].dataOrg[i][growing.valueIndex]) / data[j].grow.steps * (data[j].grow.steps - data.actualStep); 
+                        else if (growing.stepDirection === "down") {
+                            data[j].data[i][growing.valueIndex] = data[j].dataOrg[i][growing.valueIndex] + (data[j].yaxis.max - data[j].dataOrg[i][growing.valueIndex]) / data[j].grow.steps * (data[j].grow.steps - data.actualStep);
                         }
                     }
                 }
             }
-            function growMaximum(){
-                if (data.actualStep <= data[j].grow.steps){
-                    for (var i = 0; i < data[j].data.length; i++){
-                        if (growing.stepDirection === "up"){
+            function growMaximum() {
+                if (data.actualStep <= data[j].grow.steps) {
+                    for (var i = 0; i < data[j].data.length; i++) {
+                        if (growing.stepDirection === "up") {
                             data[j].data[i][growing.valueIndex] = Math.min(data[j].dataOrg[i][growing.valueIndex], data[j].yaxis.max / data[j].grow.steps * data.actualStep);
                         }
-                        else if (growing.stepDirection === "down"){
-                            data[j].data[i][growing.valueIndex] = Math.max(data[j].dataOrg[i][growing.valueIndex], data[j].yaxis.max / data[j].grow.steps * (data[j].grow.steps - data.actualStep) ); 
+                        else if (growing.stepDirection === "down") {
+                            data[j].data[i][growing.valueIndex] = Math.max(data[j].dataOrg[i][growing.valueIndex], data[j].yaxis.max / data[j].grow.steps * (data[j].grow.steps - data.actualStep));
                         }
                     }
                 }
             }
-            function growDelay(){
-                if (data.actualStep == data[j].grow.steps){
-                    for (var i = 0; i < data[j].data.length; i++){
+            function growDelay() {
+                if (data.actualStep == data[j].grow.steps) {
+                    for (var i = 0; i < data[j].data.length; i++) {
                         data[j].data[i][growing.valueIndex] = data[j].dataOrg[i][growing.valueIndex];
                     }
                 }
             }
         }
-        function clone(obj){
+
+        function clone(obj) {
             if(obj === null || typeof(obj) !== 'object'){ return obj;}
             var temp = new obj.constructor();
             for(var key in obj){temp[key] = clone(obj[key]); }
             return temp;
+        }
+
+        function onResize() {
+            if (growfunc) {
+                clearTimeout(growfunc);
+                growfunc = null;
+                plot.setupGrid();
+                plot.draw();
+            }
+        }
+
+        function shutdown(plot, eventHolder) {
+            plot.getPlaceholder().unbind("resize", onResize);
+        }
+
+        function isPluginRegistered(pluginName) {
+            var plugins = $.plot.plugins;
+
+            for (var i = 0, len = plugins.length; i < len; i++) {
+                var plug = plugins[i];
+
+                if (plug.name === pluginName) {
+                    return true;
+                }
+            }
+            return false;
         }
     }
     $.plot.plugins.push({
