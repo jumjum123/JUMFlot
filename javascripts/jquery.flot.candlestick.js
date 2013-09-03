7ccb40ed58550e7d19c752ce9b730ee634e3ba67
Juergen Marsch <juergenmarsch@googlemail.com>
2013-09-03 18:46:18 +0200

    Correct bug in candlestick.js function drawBody

--- javascripts/jquery.flot.candlestick.js
+++ javascripts/jquery.flot.candlestick.js
@@ -70,7 +70,7 @@ THE SOFTWARE.
         }
         function drawBody(ctx,serie,data){
             var x,y1,y2,c;
-            x = serie.xaxis.p2c(data[0] - serie.candlestick.barWidth / 2);
+            x = serie.xaxis.p2c(data[0]);
             y1 = serie.yaxis.p2c(data[1]);
             y2 = serie.yaxis.p2c(data[2]);
             if(data[1] > data[2]){ c = serie.candlestick.upColor;} else{ c = serie.candlestick.downColor;}
